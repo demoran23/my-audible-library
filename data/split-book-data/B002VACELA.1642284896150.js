8a09e65ee93190b7c6de4180553f2035b196a57f
Adam Skinner <adam.skinner@gmail.com>
2022-11-26 16:40:54 -0500

    21

--- data/split-book-data/B002VACELA.1642284896150.js
+++
@@ -1,1 +0,0 @@
-window.bookSummaryJSON = "The White Council of Wizards has drafted Harry Dresden as a Warden and assigned him to look into rumors of black magic in Chicago. Malevolent entities that feed on fear are loose in the Windy City, but it's all in a day's work for a wizard, his faithful dog, and a talking skull named Bob."; 
--- data/split-book-data/B07LC18BM9.1642284896150.js
+++
@@ -1,1 +0,0 @@
-window.bookSummaryJSON = "<p><b>The third book in the thrilling and epic Book of the Ancestor trilogy from international best-selling author Mark Lawrence. </b></p> <p>Powerful novice Nona Grey must fight to survive in \"a fantastic world in which religion and politics are dark and sharp as swords, with magic and might held in the hands of wonderful and dangerous women.\"</p>"; 
